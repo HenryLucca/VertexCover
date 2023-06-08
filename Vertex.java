892c67694a739db102d1fe17a8f7a6497c1739da
Henry Lucca <henryluccag@gmail.com>
2023-06-08 13:37:22 -0300

    first commit

---
+++ Vertex.java
@@ -0,0 +1,22 @@
+package ProjetoVertexCover;
+
+public class Vertex {
+
+    private int vertexId;
+
+    public Vertex(int vertexId) {
+        this.vertexId = vertexId;
+    }
+
+    public Vertex() {
+    }
+
+    public int getVertexId() {
+        return this.vertexId;
+    }
+
+    public String toString() {
+		return Integer.toString(vertexId);// returns the printing prepared version of the Vertex
+	}
+    
+}
---
+++ VertexCover.java
@@ -0,0 +1,124 @@
+// Approx-Vertex-Cover (G = (V, E))  
+// {             
+//        C = empty-set;  
+//     E'= E;  
+//     While E' is not empty do  
+//       {  
+//     Let (u, v) be any edge in E': (*)  
+//     Add u and v to C;  
+//     Remove from E' all edges incident to  
+//        u or v;  
+//       }  
+//     Return C;  
+// }
+package ProjetoVertexCover;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VertexCover {
+
+    private Graph graph;
+    private int k;
+    private int min;
+    private ArrayList<Vertex> cover;
+    ArrayList<ArrayList<Vertex>> possibleCovers = new ArrayList<ArrayList<Vertex>>();
+
+    public VertexCover(Graph graph, int k) {
+        this.graph = graph;
+        this.k = k;
+    }
+
+    public static void main(String[] args) {
+        
+        Vertex[] graphVertices = { new Vertex(1), new Vertex(2), 
+            new Vertex(3), new Vertex(4), new Vertex(5)};
+
+        Edge[] graphEdges = { 
+            new Edge(graphVertices[0], graphVertices[1]), new Edge(graphVertices[0], graphVertices[3]),
+            new Edge(graphVertices[0], graphVertices[4]), new Edge(graphVertices[1], graphVertices[2]),
+            new Edge(graphVertices[1], graphVertices[3]), new Edge(graphVertices[2], graphVertices[3]),
+            new Edge(graphVertices[2], graphVertices[4]), new Edge(graphVertices[3], graphVertices[4]) 
+        }; // the graph shown in the slides
+
+        Graph graph = new Graph(graphVertices, graphEdges);
+        System.out.println("# of Vertices: " + graph.numVertices());
+		System.out.println(graph);
+        VertexCover vc = new VertexCover(graph, 3);
+        vc.greedyCover();
+        
+        
+    }
+
+    protected void removeEdges(Vertex vertex, ArrayList<Edge> edgeList) {
+		ArrayList<Edge> removeEdge = new ArrayList<Edge>();
+
+		for (int i = 0; i < edgeList.size(); i++) {
+			if ((edgeList.get(i)).hasVertex(vertex))
+				removeEdge.add(edgeList.get(i));
+		}
+
+		for (int i = 0; i < removeEdge.size(); i++)
+			edgeList.remove(removeEdge.get(i));
+	}
+	
+	// Allows VertexCover approaches to get the Vertex that has the most associated Edges
+	protected Vertex getMaxDegree(ArrayList<Vertex> vertices, ArrayList<Edge> edges) {
+
+		Map<Vertex, Integer> map = new HashMap<Vertex, Integer>();// creates new instance to hold Vertices and their associated Vertex count
+		int max = 0;// used to hold/determine which Vertex has the highest associated Vertex count
+		Vertex maxVertex = new Vertex();// used to hold the Vertex with the highest associated Vertex count
+
+		for (Vertex vertex : vertices) {// loops through the list of remaining Vertices in the search
+			map.put(vertex,  0);// adds each Vertex to the map with an initial value of 0
+			for (Edge edge : edges) {// loops through the list of remaining Edges in the search
+				if (edge.hasVertex(vertex)) {// determines if an Edge contains a specific Vertex
+					map.put(vertex,map.get(vertex) + 1);// if the Edge contains the Vertex, the count associated with that Vertex is incremented
+				}
+			}
+			if (map.get(vertex) > max) {// determines if the current Vertex has more associated Edges than the current max
+				max = map.get(vertex);// if so, sets new max amount
+				maxVertex = vertex;// if so, sets the maxVertex to the new Vertex that has the current max associated Edges
+			}
+		}
+
+		return maxVertex;// returns the Vertex with the most associated Edges
+	}// getMaxDegree(ArrayList<Vertex>, ArrayList<Edge>)
+	
+	// Allows VertexCover approaches to get all the possible combinations of Vertex covers - recursive method
+	protected void combinations(ArrayList<Vertex> vertices, int k, ArrayList<Vertex> current, int currentTot, boolean[] visit) {
+		if (currentTot >= k-1) {// determines if the current permutation holds k or more values
+			ArrayList<Vertex> temp = new ArrayList<Vertex>();// creates a temp ArrayList<Vertex> to hold current permutation
+			for (int i = 0; i < current.size(); i++)// loops through the current permutation
+				temp.add(current.get(i));// adds each Vertex in the current permutation to the temp ArrayList<Vertex>
+			possibleCovers.add(temp);// adds the current Vertex permutation to the list of possible covers
+			return;// returns from the current iteration of the recursive method
+		}// if (currentTot)
+		// loops through the Vertices in the Graph
+		for (int i = 0; i < vertices.size(); i++) {
+			if(!visit[i]) {// determines if the ith Vertex has already been set
+				current.set(++currentTot, vertices.get(i));// if not, sets the ith Vertex in the current permutation, increments vertex count
+				visit[i] = true;// sets the ith Vertex as being set
+				combinations(vertices, k, current, currentTot, visit);// recursively calls itself with updated values
+				visit[i] = false;// resets ith Vertex to unset for next permutation
+				currentTot--;// decrements the vertex count
+			}// if (!visit)
+		}// for (int i)
+	}// combinations(ArrayList<Vertex>, int, ArrayList<Vertex>, int, boolean[]) method
+
+    protected void greedyCover() {
+		ArrayList<Edge> edges = graph.getEdges();// gets all the Edges in the Graph
+		ArrayList<Vertex> vertices = graph.getVertices();// gets all the Vertices in the Graph
+		cover = new ArrayList<Vertex>();// creates a new ArrayList<Vertex> to hold the Vertex cover
+
+		while (!edges.isEmpty()) {// loops until a Vertex cover has been found
+			Vertex vertex = getMaxDegree(vertices, edges);// gets the Vertex with the most associated Edges
+			cover.add(vertex);// adds the Vertex found above to the Vertex cover list
+			vertices.remove(vertex);// removes the Vertex found above from the list of available Vertices
+			this.removeEdges(vertex, edges);// removes all the Edges that contain the Vertex found above as an endpoint
+		}
+
+		System.out.println("The vertices of the cover are: " + cover.toString());
+	}
+}
